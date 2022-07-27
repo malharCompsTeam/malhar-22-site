3e3f2805b7448137944ca12dc4afd89751676467
Matthew Dsouza <matthewdsouza.says@gmail.com>
2022-07-28 00:46:59 +0530

    testing js module images

--- pages/workforce/js/data1.js
+++ pages/workforce/js/data1.js
@@ -1,18 +1,18 @@
 const waifus = [
   {
-    img: "../images/waifu1.jpg",
+    img: "./waifu1.jpg",
     name: "ching doe",
     job: "product manager",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
   },
   {
-    img: "../images/waifu2.jpeg",
+    img: "./waifu2.jpeg",
     name: "susan doe",
     job: "developer",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
   {
-    img: "../images/waifu3.jpeg",
+    img: "./waifu3.jpeg",
     name: "emma doe",
     job: "designer",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
