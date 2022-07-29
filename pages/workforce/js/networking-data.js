61721b6f2919380740a5c72c7e289a526dc66116
Matthew Dsouza <matthewdsouza.says@gmail.com>
2022-07-29 15:02:52 +0530

    modified slider logic for multiple

--- pages/workforce/js/networking-data.js
+++ pages/workforce/js/networking-data.js
@@ -1,23 +1,46 @@
 const networkingPeople = [
-    {
-      img: "./images/waifu7.jpeg",
-      name: "ching doe",
-      job: "product manager",
-      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
-    },
-    {
-      img: "./images/waifu8.png",
-      name: "susan doe",
-      job: "developer",
-      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
-    },
-    {
-      img: "./images/waifu9.jpeg",
-      name: "emma doe",
-      job: "designer",
-      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
-    },
-  ];
-  
-  export default networkingPeople;
-  
+  {
+    img: "./images/waifu1.jpg",
+    name: "Networking",
+    job: "product manager",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+  },
+  {
+    img: "./images/waifu2.jpeg",
+    name: "Computers",
+    job: "developer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: "./images/waifu3.jpeg",
+    name: "Conclave",
+    job: "designer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: "./images/waifu4.jpeg",
+    name: "Creatives",
+    job: "designer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: "./images/waifu5.jpeg",
+    name: "Public Relations",
+    job: "designer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: "./images/waifu6.jpeg",
+    name: "Decor & Merchandise",
+    job: "designer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: "./images/waifu7.jpeg",
+    name: "Filming & Documentation",
+    job: "designer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+];
+
+export default networkingPeople;
