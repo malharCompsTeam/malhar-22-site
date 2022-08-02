09b9bc63ea95cebff00da7289871010809a84344
sarthak <mirjisarthak3@gmail.com>
2022-08-03 00:42:56 +0530

    landing page complete & navbar mkc

--- pages/landing/script.js
+++ pages/landing/script.js
@@ -1,4 +1,34 @@
 //-----------------------------------
+// NAVBAR -------
+//-----------------------------------
+$(document).ready(function () {
+	$(window).scroll(function () {
+	  // sticky navbar on scroll script
+	  if (this.scrollY > 20) {
+		$(".navbar").addClass("sticky");
+	  } else {
+		$(".navbar").removeClass("sticky");
+	  }
+	});
+
+  
+	$(".navbar .menu li a").click(function () {
+	  // applying again smooth scroll on menu items click
+	  $("html").css("scrollBehavior", "smooth");
+	});
+  
+	// toggle menu/navbar script
+	$(".menu-btn").click(function () {
+	  $(".navbar .menu").toggleClass("active");
+	  $(".menu-btn i").toggleClass("active");
+	});
+  });
+
+  setTimeout(function () {
+	document.querySelector(".navbar").style.visibility = "visible";
+  }, 5000);
+
+//-----------------------------------
 // PRODUCTION CONTROLS SCRIPT -------
 //-----------------------------------
 
