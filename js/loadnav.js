c2225e5f4513e50448700ec637f915d924a9bbf5
sarthak <mirjisarthak3@gmail.com>
2022-08-05 22:12:16 +0530

    load nav

--- js/loadnav.js
+++ js/loadnav.js
@@ -1,4 +1,9 @@
-if(performance.navigation.type == 2)
-{
-    window.location = "index.html";
-}
+window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
+                           ( typeof window.performance != "undefined" && 
+                                window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      // Handle page restore.
+      window.location.reload();
+    }
+  })
