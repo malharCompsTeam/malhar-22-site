6187ff81dc4d27798add54589d7a032652c6e645
xxchanjotxx <chanjot10@gmail.com>
2022-08-06 12:23:27 +0530

    contact js

--- pages/contact/script.js
+++ pages/contact/script.js
@@ -1,12 +1,12 @@
-// const scriptURL = "https://script.google.com/macros/s/AKfycbzCu5pdeYEedozyD-cYMoISTxoPlaSUg97lzV3rVzByrffJGoQbfnAddZDIbFvsWRzA/exec";
-// const form = document.forms["google-sheet"];
+const scriptURL = "https://script.google.com/macros/s/AKfycbzCu5pdeYEedozyD-cYMoISTxoPlaSUg97lzV3rVzByrffJGoQbfnAddZDIbFvsWRzA/exec";
+const form = document.forms["google-sheet"];
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-//     .then((response) => alert("Thank You! Your submission has been received."))
-//     .catch((error) => console.error("Error!", error.message));
-// });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => alert("Thank You! Your submission has been received."))
+    .catch((error) => console.error("Error!", error.message));
+});
 
 //-----------------------------------
 // NAVBAR -------
