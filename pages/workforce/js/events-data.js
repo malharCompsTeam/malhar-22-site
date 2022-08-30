9c96d3de2cf1a093eaae93f4cd8b2eb271c36008
sarthak <mirjisarthak3@gmail.com>
2022-08-31 01:13:53 +0530

    final commit :') i guess

--- pages/workforce/js/events-data.js
+++ pages/workforce/js/events-data.js
@@ -1,48 +1,58 @@
 const eventsPeople = [
   {
-    img: "./images/events.webp",
+    img: "./images/E.png",
     name: "Events",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium obcaecati velit, magni quam aliquam tempora ad, ducimus incidunt delectus adipisci nihil dolore natus eveniet eligendi. Numquam iusto consectetur repellendus aut distinctio, saepe aliquam ducimus, cupiditate illum dolorem delectus, voluptatum perferendis consequatur asperiores esse. Aperiam velit dolorum deserunt numquam, accusantium maxime, esse, illum natus animi laboriosam dolor harum impedit quaerat necessitatibus fuga accusamus quos alias et recusandae laudantium quod quis officiis aut? Repudiandae asperiores, officiis nisi, nesciunt done cunt`,
+    text: `The ones who are keeping the competitive spirit of Malhar alive and
+    rerouting the legacy of Malhar's events in this never-seen-before virtual
+    setup. It's them who make Malhar as Event-ful as it gets!`,
   },
   {
     img: "./images/workshops.png",
     name: "Workshops Inc.",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    text: `Our department of Workshops Inc. wishes for everyone at Malhar to experience
+    learning alongside fun with a wide range of hands on workshops ranging from
+    art, cooking, forensics, styling, dance, music and much more. We believe in
+    aiding the broadening of every participant's horizons and enabling each one
+    to grow in multitudes of ways. WINC is a humble abode, and we will make sure
+    you have a wholesome time!`,
   },
   {
     img: "./images/ipa.png",
     name: "Indian Performing Arts",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    text: `Indian Performing Arts believes in staying desi at heart and keeping alive the dynamic and multi-hued culture and art forms of our country. The events will be no less than a powerhouse of talent from magical musical performances, and dazzling dances to eye-opening streetplays. Keep the hearts beating cause it's time for the desi beat!`,
   },
   {
     img: "./images/wpa.png",
     name: "World Performing Arts",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    text: `Welcome to the bright side! Overflowing with energy and expression, here’s introducing a riot of unparalleled talent: The World Performing Arts Department. From lyrics to gimmicks, to soulful pirouettes, our events aim to reflect the perfect melange of innovation and emotion. Turn up that bass, grab your headphones and get your groove on because it’s time to hop on to this expedition of unconstrained, limitless creativity.`,
   },
   {
     img: "./images/la.png",
     name: "Literary Arts",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    text: `Have you ever transcended space and time to enter different literary universes?
+    If yes, Literary Arts (LA) is the perfect fit for you! A hub of all things creative, witty, and spontaneous, LA is the place to be if you’re a writer, public speaker, or dreamer. 
+    "I envision a place where creativity sings, and expression gives you wings".
+    <br/>
+    Well, if that is the case,
+    <br/>
+    Welcome to your safe space.
+    `,
   },
   {
     img: "./images/fa.png",
     name: "Fine Arts",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    text: `Stuck between reality and a surrealist dream, Fine Arts is an Events department of dedicated people working in a constant state of paint-filled panic. Besides organizing engaging events, FA also curates a homegrown Art Lounge (psst its in LR 22), the theme for which is ‘Art My Weirdest: Dadaism x Absurdism’ this year! Led by an all-female core team, the FA department of Malhar 2022 ‘blends the rules’ to guarantee you the perfect dose of creativity, chaos and colour.`,
   },
   {
     img: "./images/etc.png",
     name: "Entertainment, Theatrics & Contests",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    text: `Perfunctorily call us dysfunctional,malfunctional or even unfunctional, you can’t really miss the ‘fun’ in it. Here at ETC, we don’t believe in fitting in. In fact, many people read ETC as etcetera, which is perfect, because why define when you can just be! If you’re someone who’s fed up with the mainstream, quirky, vivacious, funny, etcetera, this is just the place for you.`,
   },
   {
     img: "./images/admin.png",
     name: "Admin",
-    text: `The rulebook creator of Malhar- Admin, works in the shadows to ensure the
-    smooth functioning of this fest. The baby of all the sub-departments i.e.
-    Contingency Communication is the newest addition to the department. When in
-    doubt, Admin is the department to fall back on. Being the most trustworthy
-    of all, Admin abides by the motto "The show, in case of Malhar, the grand
-    show, must go on."`,
+    text: `The Backbone of Malhar: Admin keeps the Malhar Machine running smoothly and declares the final winners of Malhar. When in doubt, Admin is the department to fall back on. Being the Rulebook Creator of Malhar, Admin likes to go by their tagline: “You Play By Our Rules.”
+    Be assured that this power packed bunch will have your back!`,
   },
 ];
 
